ae565638d5308471b306a45514874afecd309506
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-11-25 09:40:54 +0700

    add api app gettoken

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   // urlApi : 'https://45.124.94.191:8019/api/',
   // urlApi : 'https://seaportwebapi.phanmem.one/api/',
   urlApi : 'https://localhost:8080/api/',
+  urlApi2 : 'http://45.124.94.191:8789/',
   // urlApi : 'http://10.38.196.211:8020/api/',
 };
 
