01871c9368629b502bfe0629ca20e90a6857a757
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-12-08 16:46:31 +0700

    Add file config prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    // độ rộng của tab, tính theo Space
+    // default: 2
+    tabWidth: 4,
+
+    // độ dài của dòng, nếu quá sẽ xuống dòng
+    printWidth: 120,
+
+    // Thêm dấu cách giữa các dấu ngoặc trong các ký tự đối tượng
+    // default: true
+    bracketSpacing: false,
+
+    // Thêm dấu chấm phẩy ở cuối câu lệnh
+    // default: true
+    semi: true,
+
+    // Sử dụng dấu ngoặc đơn thay vì dấu ngoặc kép
+    // default: false
+    singleQuote: true,
+    jsxSingleQuote: true,
+
+    //Thêm dấu phẩy ở cuối list
+    // default:
+    trailingComma: 'all',
+
+    // Đặt dấu> của một phần tử JSX nhiều dòng ở cuối dòng cuối cùng thay vì ở một mình trên dòng tiếp theo (không áp dụng cho các phần tử tự đóng).
+    // default: false
+    bracketSameLine: true,
+    jsxBracketSameLine: true,
+
+    // Bao gồm dấu ngoặc đơn xung quanh tham số hàm mũi tên duy nhất
+    // default: always
+    // arrowParens: 'avoid',
+};
