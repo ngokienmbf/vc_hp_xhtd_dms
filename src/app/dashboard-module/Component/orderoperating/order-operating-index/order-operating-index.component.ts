211edfff1b87bd75a68a0b07b786ee01e88e44ef
dam manh <damvanmanh4495@gmail.com>
2022-12-15 08:14:53 +0700

    edit ui

--- src/app/dashboard-module/Component/orderoperating/order-operating-index/order-operating-index.component.ts
+++ src/app/dashboard-module/Component/orderoperating/order-operating-index/order-operating-index.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-order-operating-index',
-  templateUrl: './order-operating-index.component.html',
-  styleUrls: ['./order-operating-index.component.css']
+    selector: 'app-order-operating-index',
+    templateUrl: './order-operating-index.component.html',
+    styleUrls: ['./order-operating-index.component.css']
 })
 export class OrderOperatingIndexComponent implements OnInit {
+    loading: boolean = false;
+    selected: number = 0;
+    constructor() { }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    myTabSelectedIndexChange(index: number) {
+        this.selected = index;
+    }
 
 }
